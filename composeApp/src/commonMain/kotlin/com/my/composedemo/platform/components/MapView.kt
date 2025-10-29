814ff522400e93a8abe1d94c8ef29db4648c872a
makoto yasuda <hare.v1v1enne@gmail.com>
2025-10-29 15:55:51 +0900

    update: map

--- composeApp/src/commonMain/kotlin/com/my/composedemo/platform/components/MapView.kt
+++ composeApp/src/commonMain/kotlin/com/my/composedemo/platform/components/MapView.kt
@@ -6,6 +6,9 @@ import androidx.compose.ui.Modifier
 @Composable
 expect fun MapView(
     modifier: Modifier = Modifier,
+    latitude: Double,
+    longitude: Double,
+    zoom: Float,
     onMapClick: (() -> Unit)? = null
 )
 
